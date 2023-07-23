26d8aef599500986e6d80eb927b69dab8453ef26
Pascal Dennerly <dnnrly@gmail.com>
2023-07-23 22:55:06 +0100

    Check that the demo image corresponds to the input

--- docs_test.go
+++ docs_test.go
@@ -1,7 +1,10 @@
+//go:build docs
+
 package layli_test
 
 import (
 	"io/ioutil"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +19,14 @@ func TestReadme_Updated(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Contains(t, string(readme), string(demo))
+}
+
+func TestReadme_ImageUpToDate(t *testing.T) {
+	err := exec.Command("./layli", "./demo.layli", "--show-grid").Run()
+	require.NoError(t, err)
+
+	result, err := exec.Command("git", "status", "demo.svg").Output()
+	require.NoError(t, err)
+
+	assert.NotContains(t, string(result), "demo.svg")
 }
