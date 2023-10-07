230572c476f16dd8307733fd229d64e3afad57f5
Pascal Dennerly <dnnrly@gmail.com>
2023-10-07 23:17:13 +0100

    Implement dijkstra with deterministic paths

--- pathfinder/dijkstra/djikstra_test.go
+++ pathfinder/dijkstra/djikstra_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"testing"
 
-	dj "github.com/RyanCarrier/dijkstra"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -47,6 +46,30 @@ func TestAddManyVertices(t *testing.T) {
 	)
 }
 
+func TestPathFinderWithCycles(t *testing.T) {
+	pf := NewPathFinder(p(1, 1), p(4, 4))
+
+	cost := func(from, to Point) int64 { return 1 }
+
+	pf.AddConnection(p(1, 1), cost, p(1, 2))
+	pf.AddConnection(p(1, 2), cost, p(1, 3), p(2, 2))
+	pf.AddConnection(p(1, 3), cost, p(1, 4), p(2, 3))
+	pf.AddConnection(p(2, 2), cost, p(3, 2))
+	pf.AddConnection(p(2, 3), cost, p(3, 3))
+	pf.AddConnection(p(3, 2), cost, p(4, 2))
+	pf.AddConnection(p(3, 3), cost, p(4, 3))
+	pf.AddConnection(p(4, 2), cost, p(4, 3))
+	pf.AddConnection(p(4, 3), cost, p(4, 4))
+
+	shortestPath, err := pf.BestPath()
+
+	assert.NoError(t, err)
+	assert.Equal(t,
+		fmt.Sprintf("%v", []Point{p(1, 1), p(1, 2), p(2, 2), p(3, 2), p(4, 2), p(4, 3), p(4, 4)}),
+		fmt.Sprintf("%v", shortestPath),
+	)
+}
+
 func TestCannotFindImpossiblePath(t *testing.T) {
 	pf := NewPathFinder(p(1, 1), p(2, 2))
 
@@ -56,6 +79,5 @@ func TestCannotFindImpossiblePath(t *testing.T) {
 	pf.AddConnection(p(3, 2), cost, p(2, 2))
 
 	_, err := pf.BestPath()
-	assert.Contains(t, err.Error(), "calculating shortest path")
-	assert.ErrorIs(t, err, dj.ErrNoPath)
+	assert.Contains(t, err.Error(), "no path found")
 }
