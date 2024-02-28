53a301c76cb2fa358203c2ade6c1759865086844
Pascal Dennerly <dnnrly@gmail.com>
2024-02-28 23:00:27 +0000

    Read in basic node and edge config

--- layli.go
+++ layli.go
@@ -1,10 +1,12 @@
 package layli
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 
 	svg "github.com/ajstarks/svgo"
-	"gopkg.in/yaml.v3"
+	"github.com/antchfx/xmlquery"
 )
 
 type OutputFunc func(output string) error
@@ -54,15 +56,44 @@ func (d *Diagram) Draw() error {
 // AbsuluteFromSVG parses a string of an SVG and turns it in to a Layli configuration
 // with with absulute layout that can represent the same SVG
 func AbsoluteFromSVG(svg string, output OutputFunc) error {
-	config := &Config{
-		Nodes: ConfigNodes{
-			ConfigNode{Id: "1", Position: Position{X: 3, Y: 3}},
-			ConfigNode{Id: "2", Position: Position{X: 3, Y: 7}},
-		},
+	dom, err := xmlquery.Parse(strings.NewReader(svg))
+	if err != nil {
+		return fmt.Errorf("parsing svg: %w", err)
 	}
 
-	buf := strings.Builder{}
-	yaml.NewEncoder(&buf).Encode(config)
+	config := &Config{
+		Nodes: ConfigNodes{},
+	}
 
-	return output(buf.String())
+	for _, n := range xmlquery.Find(dom, "//rect") {
+		id := n.SelectAttr("id")
+		x, err := strconv.Atoi(n.SelectAttr("data-pos-x"))
+		if err != nil {
+			return fmt.Errorf("parsing X: %w", err)
+		}
+		y, err := strconv.Atoi(n.SelectAttr("data-pos-y"))
+		if err != nil {
+			return fmt.Errorf("parsing Y: %w", err)
+		}
+
+		text := xmlquery.FindOne(dom, "//*[@id='"+id+"-text']")
+		if text == nil {
+			return fmt.Errorf("no text found for node %s", id)
+		}
+
+		config.Nodes = append(config.Nodes, ConfigNode{
+			Id:       id,
+			Contents: text.InnerText(),
+			Position: Position{X: x, Y: y},
+		})
+	}
+
+	for _, e := range xmlquery.Find(dom, "//g/path") {
+		config.Edges = append(config.Edges, ConfigEdge{
+			From: e.SelectAttr("data-from"),
+			To:   e.SelectAttr("data-to"),
+		})
+	}
+
+	return output(config.String())
 }
